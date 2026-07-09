9a361f84d86b5b8c82b166630200214aabd05c34
gorgalikhanzebel-alt <gorgali.khan.zebel@gmail.com>
2026-07-09 07:20:35 +0330

    Update database.js

--- services/database.js
+++ services/database.js
@@ -1,16 +1,5 @@
-const mongoose = require('mongoose');
-
 async function connectDB() {
-  // اگر متغیر محیطی نبود، از این آدرس مستقیم استفاده کن
-  const uri = process.env.MONGODB_URI || 'mongodb+srv://gorgalikhanzebel_db_user:ZZ8O2D3VhzPgqHxZ@cluster0.y56mkjn.mongodb.net/?appName=Cluster0';
-  
-  try {
-    await mongoose.connect(uri);
-    console.log('✅ به MongoDB متصل شدیم!');
-  } catch (error) {
-    console.error('❌ خطا در اتصال به MongoDB:', error);
-    process.exit(1);
-  }
+  const uri = process.env.MONGODB_URI || '...';
+  await mongoose.connect(uri);
+  console.log('✅ به MongoDB متصل شدیم!');
 }
-
-module.exports = { connectDB };
