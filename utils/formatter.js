6e35821214339ea0736f0b192600a9819db148cd
gorgalikhanzebel-alt <gorgali.khan.zebel@gmail.com>
2026-07-09 08:27:39 +0330

    Delete utils directory

--- utils/formatter.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  bold: (text) => `*${text}*`,
-  italic: (text) => `_${text}_`,
-  code: (text) => `\`${text}\``,
-  link: (text, url) => `[${text}](${url})`
-};
--- utils/validator.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  isNumeric: (value) => /^\d+$/.test(value),
-  isEmail: (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email),
-  isUrl: (url) => /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(url)
-};
