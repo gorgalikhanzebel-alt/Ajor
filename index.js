e8f187a0d2579cab6b84b1471a08ebfe9b7012f4
gorgalikhanzebel-alt <gorgali.khan.zebel@gmail.com>
2026-07-09 07:18:25 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('ربات فعال است');
-});
-
-app.listen(3000, () => {
-  console.log('🚀 ربات روی پورت 3000 اجرا شد');
-});
+const config = require('./config/settings');
+const { connectDB } = require('./services/database');
+const { sendMessage } = require('./services/telegram');
+const handleCommand = require('./handlers/commands');
