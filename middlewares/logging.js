eaae7cce3219f176ff4cc49ea328eb5b9706bc55
gorgalikhanzebel-alt <gorgali.khan.zebel@gmail.com>
2026-07-09 08:26:55 +0330

    Delete middlewares directory

--- middlewares/logging.js
+++
@@ -1,9 +0,0 @@
-const logger = require('../utils/logger');
-
-module.exports = function loggingMiddleware(req, res, next) {
-  const { message } = req.body;
-  if (message) {
-    logger.log(`📩 پیام از ${message.from.id}: ${message.text || 'فایل'}`);
-  }
-  next();
-};
